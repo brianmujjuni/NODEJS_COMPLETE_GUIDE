4c66425433091fdbc8a9922feff72d2eeffe8d4a
Mujjuni Brian <brianmujjuni@engineer.com>
2024-06-15 16:22:31 +0300

    loop all products in shop pug template

--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,32 @@ const rootDir = require("../uitl/path");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render('shop')
+  const products = [
+    {
+      name: "Airpods Wireless Bluetooth Headphones",
+      image: "/images/airpods.jpg",
+      description:
+        "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality audio and bass audio",
+      brand: "Apple",
+      category: "Electronics",
+      price: 89.99,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 12,
+    },
+    {
+      name: "iPhone 11 Pro 256GB Memory",
+      image: "/images/phone.jpg",
+      description:
+        "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+      brand: "Apple",
+      category: "Electronics",
+      price: 599.99,
+      countInStock: 7,
+      rating: 4.0,
+      numReviews: 8,
+    },
+  ];
+  res.render("shop", { products, prodTitle: "Automex" });
 });
 module.exports = router;
