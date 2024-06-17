79cc4a67f086d7d6df39ccba7d7dd9cf387bf7f7
Mujjuni Brian <brianmujjuni@engineer.com>
2024-06-17 18:51:40 +0300

    extend main layou page to shop page

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const rootDir = require("../uitl/path");
 const router = express.Router();
 
 router.get("/add-product", (req, res) => {
-  res.render("add-product");
+  res.render("add-product", { path: "/add-product" });
 });
 
 router.post("/product", (req, res) => {
