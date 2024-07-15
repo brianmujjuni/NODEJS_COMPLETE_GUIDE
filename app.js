e8de9ba90c5c6f38d5f42308a58836106245e55e
Mujjuni Brian <brianmujjuni@engineer.com>
2024-07-15 16:02:32 +0300

    add products to cart and delete items from the cart as well

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const sequelize = require("./uitl/database");
 //models
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -36,6 +38,10 @@ app.use(errorController.get404);
 //relations
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   //force isn't recommended in production
@@ -53,6 +59,8 @@ sequelize
       });
     }
     return user;
+  }).then((user) => {
+    return user.createCart();
   })
   .then((user) => {
     app.listen(3000);
