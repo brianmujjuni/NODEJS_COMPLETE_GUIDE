fe9693c0100a8eda504ffce207c66d8f9f9cfc7b
Mujjuni Brian <brianmujjuni@engineer.com>
2024-07-17 18:02:31 +0300

    add quantity to order

--- models/order.js
+++ models/order.js
@@ -1,11 +1,12 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../uitl/database');
-const Order = sequelize.define('order', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    }
+const Sequelize = require("sequelize");
+const sequelize = require("../uitl/database");
+const Order = sequelize.define("order", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  quantity: Sequelize.INTEGER,
 });
-module.exports = Order
+module.exports = Order;
