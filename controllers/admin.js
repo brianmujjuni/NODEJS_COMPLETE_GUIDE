4fa848d68f121182bc25df17e63673e311263651
Mujjuni Brian <brianmujjuni@engineer.com>
2024-07-18 19:35:44 +0300

    restracture product update

--- controllers/admin.js
+++ controllers/admin.js
@@ -51,14 +51,15 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  Product.findByPk(prodId)
-    .then((product) => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  );
+  product
+    .save()
     .then((result) => {
       console.log("updated");
       res.redirect("/admin/products");
