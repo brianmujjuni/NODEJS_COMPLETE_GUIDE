cdc56e4e40913f84107c269b3776da4f7dc49a88
Mujjuni Brian <brianmujjuni@engineer.com>
2024-07-18 10:20:57 +0300

    remove sequelise model from product model

--- app.js
+++ app.js
@@ -4,30 +4,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./uitl/database");
-//models
+const mongoConnect = require("./uitl/database").mongoConnect;
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const adminRoutes = require("./routes/admin");
-// const shopRoutes = require("./routes/shop");
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-//add user to every route or request
+
 app.use((req, res, next) => {
-  //user middleware
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next();
 });
-// app.use("/admin", adminRoutes);
+
+// app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
-//relations
 
-mongoConnect((client) => {
-  console.log(client);
-  app.listen(3000, console.log("listening on port 3000"));
+mongoConnect(() => {
+  app.listen(3000);
 });
