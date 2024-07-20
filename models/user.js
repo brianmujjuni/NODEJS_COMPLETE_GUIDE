509152d7a04f602d4a68102436d1d9091e28c29f
Mujjuni Brian <brianmujjuni@engineer.com>
2024-07-20 13:06:53 +0300

    add storing multiple products to cart

--- models/user.js
+++ models/user.js
@@ -32,11 +32,27 @@ class User {
 
   addToCart(product) {
     const db = getDb();
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
 
-    // const updatedCart = { items: [{ ...product, quantity: 1 }] };
-    const updatedCart = {
-      items: [{productId: new ObjectId(product._id), quantity: 1}],
+    let newQuantity = 1;
+
+    const updateCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updateCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updateCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity,
+      });
     }
+
+    const updatedCart = {
+      items: updateCartItems,
+    };
     return db
       .collection("users")
       .updateOne(
