04bf51fd061c4330c6d278dfaa5e816356e2381f
Mujjuni Brian <brianmujjuni@engineer.com>
2024-07-25 19:54:10 +0300

    fetch cart and populate products

--- controllers/shop.js
+++ controllers/shop.js
@@ -43,8 +43,9 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then((products) => {
+    .populate("cart.items.productId")
+    .then((result) => {
+      const products = result.cart.items;
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
