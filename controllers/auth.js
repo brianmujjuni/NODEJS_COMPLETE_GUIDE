b314896b2b945a4cce3b589febf0aa0ae78b054e
Brian <mbrian@projectcode.ug>
2024-08-08 18:00:51 +0300

    set cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,5 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn =
-    req.get("Cookie").split(";")[0].trim().split("=")[0];
-  console.log(isLoggedIn);
+  const isLoggedIn = req.get('Cookie').split('=')[1]
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
