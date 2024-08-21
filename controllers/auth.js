82eae06f8f053d8277cecbb3eaae927d8c56f41b
Brian <mbrian@projectcode.ug>
2024-08-21 10:36:29 +0300

    add validation logic in login

--- controllers/auth.js
+++ controllers/auth.js
@@ -52,11 +52,35 @@ exports.getSignup = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
   const { email, password } = req.body;
+
+  const errors = validationResult(req)
+
+  if(!errors.isEmpty()){
+    return res.status(422).render('auth/login',{
+      path: '/login',
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg,
+      oldInput:{
+        email,
+        password
+      },
+      validationErrors: errors.array()
+    })
+  }
+
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        req.flash("error", "Invalid email or password");
-        return res.redirect("/login");
+        return res.status(422).render('auth/login',{
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password',
+          oldInput:{
+            email,
+            password
+          },
+          validationErrors: []
+        })
       }
       bcrypt
         .compare(password, user.password)
@@ -68,8 +92,16 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
-          req.flash("error", "Invalid email or password");
-          res.redirect("/login");
+          return res.status(422).render('auth/login',{
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: 'Invalid email or password',
+            oldInput:{
+              email,
+              password
+            },
+            validationErrors: []
+          })
         })
         .catch((err) => {
           console.log(err);
