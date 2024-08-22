a0e8594c0121200785c8e35ee2a34c464c94fbe4
Brian <mbrian@projectcode.ug>
2024-08-22 15:47:03 +0300

    change error handling in session middleware

--- app.js
+++ app.js
@@ -45,6 +45,12 @@ app.use(
 app.use(csrfProtection)
 app.use(flash())
 
+app.use((req,res,next)=>{
+  res.locals.isAuthenticated = req.session.isLoggedIn
+  res.locals.csrfToken = req.csrfToken()
+  next()
+})
+
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -60,15 +66,10 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => {
-      throw new Error(err)
+     next(new Error(err))
     });
 });
 
-app.use((req,res,next)=>{
-  res.locals.isAuthenticated = req.session.isLoggedIn
-  res.locals.csrfToken = req.csrfToken()
-  next()
-})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
